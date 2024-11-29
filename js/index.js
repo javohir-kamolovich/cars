edd446406cf7db93ede0abc9f8af9f977091ef6f
javohir-kamolovich <narzikulovjavoxir587@gmail.com>
2024-11-29 23:46:45 +0500

    start

---
+++ js/index.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+  theme: {
+    container: {
+      center: true,
+      width: {
+        desktop: "1312px",
+      },
+      padding: {
+        DEFAULT: "30px",
+        desktop: "0",
+      },
+    },
+    screens: {
+      desktop: "1280px",
+    },
+  },
+};
